c71faa6d71ff3ab29b9373aeaa6456c0f2e5ee61
aaren <popstarfreas2@gmail.com>
2021-03-21 20:41:14 +0000

    Move node_modules as part of build

--- build.plugin.js
+++ build.plugin.js
@@ -3,4 +3,5 @@ var child_process = require("child_process");
 
 child_process.execSync("npm i ../../pluginreference")
 child_process.execSync("npm run build")
+fs.renameSync("./node_modules", "./plugin/node_modules")
 fs.renameSync("./build", "./plugin")
