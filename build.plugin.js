5a706d9eb2dc9f1047bf2a5b2e0f58fe84c01a78
popstarfreas <popstarfreas2@gmail.com>
2025-12-16 22:41:55 +0000

    build: Attempt #5

--- build.plugin.js
+++ build.plugin.js
@@ -1,6 +1,8 @@
 import fs from "node:fs";
 import child_process from "node:child_process";
 
+console.log("Running `bash ./deploy.sh`")
+child_process.execSync("bash ./deploy.sh", { stdio: "inherit" })
 console.log("Running `pnpm i`")
 child_process.execSync("pnpm i", { stdio: "inherit" })
 console.log("Running `pnpm remove terrariaserver-lite`")
@@ -9,8 +11,6 @@ console.log("Running `pnpm add ../../pluginreference`")
 child_process.execSync("pnpm add ../../pluginreference", { stdio: "inherit" })
 console.log("Running `pnpm build`")
 child_process.execSync("pnpm build", { stdio: "inherit" })
-console.log("Running `bash ./deploy.sh`")
-child_process.execSync("bash ./deploy.sh", { stdio: "inherit" })
 console.log("Renaming build to plugin")
 fs.renameSync("./build", "./plugin")
 console.log("Renaming node_modules to plugin/node_modules")
